42973ccc289e0edc26f3cdb339c6ee7cb843a66a
Sagar <koiralasamir633@gmail.com>
2021-09-23 13:33:03 +0545

    includes DHT22 and lora test code

---
+++ LoRa_test/LoRa_test.ino
@@ -0,0 +1,53 @@
+#include <SPI.h>
+#include <LoRa.h>
+
+int counter = 0;
+unsigned long lasttime = 0;
+int interval = 0;
+
+void receivepckt(int pcktsize){
+  if (pcktsize) {
+    // received a packet
+    Serial.print("Received message: ");
+
+    // read packet
+    while (LoRa.available()) {
+     Serial.print((char)LoRa.read());
+      // Serial.print(LoRa.read());
+
+    }
+
+    // print RSSI of packet
+    Serial.print(" with RSSI ");
+    Serial.println(LoRa.packetRssi());
+    }
+}
+
+void setup() {
+  // LoRa.setPins(7, 8, 3);   //uncomment for receiver board
+  Serial.begin(9600);
+  while (!Serial);  
+
+  Serial.println("LoRa Receiver");
+
+  if (!LoRa.begin(433E6)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+}
+
+void loop() {
+  if (millis() - lasttime > interval){
+    Serial.print("Sending  message: Packet no: ");
+    Serial.println(counter);
+    LoRa.beginPacket();
+    LoRa.print("HELLO FROM TRANSMITTER, Packet no: ");
+    LoRa.print(counter);
+    LoRa.endPacket();
+    lasttime = millis();
+    interval = random(1000)+800;
+    counter++;
+    Serial.println();
+  }
+receivepckt(LoRa.parsePacket());
+}
--- RTC_ds3231_test/RTC_ds3231_test.ino
+++ RTC_ds3231_test/RTC_ds3231_test.ino
@@ -1,3 +1,4 @@
+// Date and time functions using a DS3231 RTC connected via I2C and Wire lib
 #include "RTClib.h"
 
 RTC_DS3231 rtc;
@@ -5,7 +6,7 @@ RTC_DS3231 rtc;
 char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
 void setup () {
-  Serial.begin(57600);
+  Serial.begin(9600);
 
 #ifndef ESP8266
   while (!Serial); // wait for serial port to connect. Needed for native USB
@@ -19,24 +20,19 @@ void setup () {
 
   if (rtc.lostPower()) {
     Serial.println("RTC lost power, let's set the time!");
-    // When time needs to be set on a new device, or after a power loss, the
-    // following line sets the RTC to the date & time this sketch was compiled
     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-    // This line sets the RTC with an explicit date & time, for example to set
-    // January 21, 2014 at 3am you would call:
-    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
   }
 
-  // When time needs to be re-set on a previously configured device, the
-  // following line sets the RTC to the date & time this sketch was compiled
-  // rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  // rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));    //adjust time on previously configured device
+
+
   // This line sets the RTC with an explicit date & time, for example to set
   // January 21, 2014 at 3am you would call:
   // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
 }
 
 void loop () {
-    DateTime now = rtc.now();
+    DateTime now = rtc.now()+ TimeSpan(0, 0, 0, 7);
 
     Serial.print(now.year(), DEC);
     Serial.print('/');
@@ -53,10 +49,16 @@ void loop () {
     Serial.print(now.second(), DEC);
     Serial.println();
 
+    Serial.print(" since midnight 1/1/1970 = ");
+    Serial.print(now.unixtime());
+    Serial.print("s = ");
+    Serial.print(now.unixtime() / 86400L);
+    Serial.println("d");
+
     Serial.print("Temperature: ");
     Serial.print(rtc.getTemperature());
     Serial.println(" C");
 
     Serial.println();
-    delay(3000);
+    delay(100);
 }
