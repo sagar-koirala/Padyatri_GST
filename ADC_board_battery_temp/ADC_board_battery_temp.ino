34e38c50e7b7f7de486fc1aca33ef2e11061513c
Sagar <koiralasamir633@gmail.com>
2021-09-23 19:19:54 +0545

    battery temperature, analog read modified

--- ADC_board_battery_temp/ADC_board_battery_temp.ino
+++ ADC_board_battery_temp/ADC_board_battery_temp.ino
@@ -16,7 +16,8 @@ void setup() {
 void loop() {
   raw_battery_temp = analogRead(battery_temp_pin);
   raw_an_read = analogRead(an_read_pin);
-  battery_temp = map(raw_battery_temp,0,1023,0,100);
+  float voltage = raw_battery_temp * (3300 / 1024.0);
+  battery_temp = voltage/10;
   an_read = map(raw_an_read,0,1023,0,255);
 
   Serial.println("                   ----Raw readings----  |  ----Real values----");
@@ -24,7 +25,8 @@ void loop() {
   Serial.print(raw_battery_temp);
   Serial.print("                       ");
   Serial.print(battery_temp);
-  Serial.println("*C");
+  Serial.print(" \xC2\xB0");
+  Serial.println("C");
 
   Serial.print("Analog_read_pin:           ");
   Serial.print(raw_an_read);
